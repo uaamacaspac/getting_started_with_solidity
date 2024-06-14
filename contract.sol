b07f10fca573759d53c1ce925e3d5fc19b93b322
uaamacaspac <145097800+uaamacaspac@users.noreply.github.com>
2024-06-14 21:30:44 +0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+contract MyToken {
+    // Public variables here
+    string public name;
+    string public abbreviation;
+    uint public totalSupply;
+
+    // Mapping variable here
+    mapping(address => uint) public addressBalances;
+
+    // Constructor to initialize the token details
+    constructor(string memory _name, string memory _abbreviation, uint _initialSupply) {
+        name = _name;
+        abbreviation = _abbreviation;
+        totalSupply = _initialSupply;
+        addressBalances[msg.sender] = _initialSupply;
+    }
+
+    // Mint function
+    function mint(address recipient, uint value) external {
+        totalSupply += value;
+        addressBalances[recipient] += value;
+    }
+
+    // Burn function
+    function burn(address sender, uint value) external {
+        require(addressBalances[sender] >= value, "Insufficient balance to burn");
+        totalSupply -= value;
+        addressBalances[sender] -= value;
+    }
+}
